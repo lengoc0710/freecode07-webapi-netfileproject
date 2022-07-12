608dd87b1c6b523732c384e0e7d77aac32e528fe
NGOC LE BAO <quanngoc2026@gmail.com>
2022-07-12 18:24:11 +0700

    Add Entity, EFcore, API, Repository

---
+++ MyWebAPIApp/MyWebAPIApp/Data/HangHoa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebAPIApp.Data
+{
+    [Table("HangHoa")]
+    public class HangHoa
+    {
+        [Key]
+        public Guid MaHangHoa { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Tenhh { get; set; }
+
+        public string Mota { get; set; }
+        [Range(0, double.MaxValue) ]
+        public double DonGia { get; set; }
+
+        public byte GiamGia { get; set; }
+        public int? MaLoai { get; set; }
+        [ForeignKey("MaLoai")]
+        public Loai Loai { get; set; }
+        public ICollection<DonHangChiTiet> DonHangChiTiets { set; get; }
+        public HangHoa()
+        {
+            DonHangChiTiets = new HashSet<DonHangChiTiet>();
+        }
+    }
+}
---
+++ MyWebAPIApp/MyWebAPIApp/Models/HangHoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebAPIApp.Models
+{
+    public class HangHoaVm
+    {
+        public string TenHangHoa { get; set; }
+        public double DonGia { get; set; }
+    }
+    public class HangHoa : HangHoaVm
+    {
+        public Guid MaHangHoa { get; set; }
+    }
+}
